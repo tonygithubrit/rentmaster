f4c22fc4156e81237a995078659589a5aba07520
tonygithubrit <teejayilah@gmail.com>
2026-03-10 02:01:00 +0100

    updated

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -21,8 +21,8 @@ const generateOTP = () => {
 const sendVerificationEmail = async (email, name, otp) => {
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
